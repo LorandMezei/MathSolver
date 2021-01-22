69e9bdfa299c550f75a61258cd12cb2c1886dfcf
Lorand Mezei <lorand_mezei@yahoo.com>
2021-01-21 21:48:13 -0500

    Initial PrefixPrint commit.

--- src/lorandmezei/mathsolver/PrefixPrint.kt
+++ src/lorandmezei/mathsolver/PrefixPrint.kt
@@ -1,7 +1,36 @@
 package lorandmezei.mathsolver
 
-class PrefixPrint : IVisitor {
-    override fun visit(node: INode): Any {
-        TODO("Not yet implemented")
+class PrefixPrint : IVisitor
+{
+    override fun visit(node: INode): Any?
+    {
+        // Base case I think.
+        if (node == null)
+        {
+            return null
+        }
+
+        // If the current node is an operator.
+        if (node.leftNode != null && node.leftNode != null)
+        {
+            when(node.content)
+            {
+                '*' -> println("mul ")
+                '/' -> println("div ")
+                '+' -> println("add ")
+                '-' -> println("sub ")
+            }
+
+            // Recursion.
+            node.leftNode.accept(this)
+            node.rightNode.accept(this)
+        }
+
+        else
+        {
+            println(node.content)
+        }
+
+        return ""
     }
 }
