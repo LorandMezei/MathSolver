71f12aa74975a3658d0cbf7bc83103fac1d343ba
Lorand Mezei <lorand_mezei@yahoo.com>
2021-01-21 19:35:59 -0500

    Set up initial methods in ITree.

--- src/lorandmezei/mathsolver/ITree.kt
+++ src/lorandmezei/mathsolver/ITree.kt
@@ -4,5 +4,9 @@ interface ITree {
     // Set root node.
     fun setRootNode(rootNode : INode)
 
+    // Get root node.
     fun getRootNode(): INode
+
+    // Traverse the tree.
+    fun traverse(visitor : IVisitor)
 }
