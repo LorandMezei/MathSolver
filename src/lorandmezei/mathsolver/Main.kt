96236db2c10e24077e446bbaf3396e6295fb2c88
Lorand Mezei <lorand_mezei@yahoo.com>
2021-02-27 15:33:31 -0500

    Turn user input String to Array<String>

--- src/lorandmezei/mathsolver/Main.kt
+++ src/lorandmezei/mathsolver/Main.kt
@@ -1,5 +1,7 @@
 package lorandmezei.mathsolver
 
+// Sources: https://www.techiedelight.com/add-new-element-array-kotlin/
+
 fun main()
 {
     /**
@@ -11,8 +13,20 @@ fun main()
      *
      */
 
-    var input: String = "1^2*3/4+5-6"
-    var expression: Array<String> = arrayOf("1","^","2","*","3","/","4","+","5","-","6")
+    // Create expression from user input---------------------------------------
+    var input: String = "1^2*3/4+5-6" // readLine()!!
+    var chars: CharArray = input.toCharArray()
+
+    var inputArray: Array<String> = arrayOf()
+    var list: MutableList<String> = inputArray.toMutableList()
+
+    for (i in chars)
+    {
+        list.add(i.toString())
+    }
+
+    var expression: Array<String> = list.toTypedArray() //arrayOf("1","^","2","*","3","/","4","+","5","-","6")
+    //-------------------------------------------------------------------------
 
     // Build the expression tree. Pass the expression (a character array), and the Tree object
     // to create it in.
